c04cab4bcb94a99c276ec832be31c236fc596ee0
Dorota Borowska <youcode2021db@gmail.com>
2021-05-21 23:42:29 +0200

    js refactoring

--- js/calc.js
+++ js/calc.js
@@ -1,43 +1,53 @@
-console.log("Hello!");
 {
-    let formElement = document.querySelector(".js-form");
-    let amountElement = document.querySelector(".js-amount");
-    let currencySelectElement = document.querySelector(".js-currencySelect");
+    const exchangeRateElement = document.querySelector(".js-exchangeRate");
+    const resultElement = document.querySelector(".js-result");
 
-    let exchangeRateElement = document.querySelector(".js-exchangeRate");
-    let resultElement = document.querySelector(".js-result");
-
-    let rateEUR = 4.5316;
-    let rateUSD = 3.7374;
-    let rateGBP = 5.2571;
-    let exchangeRate;
-    let result;
-
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        let currency = currencySelectElement.value;
+    const selectCurrency = (currency) => {
+        const rateEUR = 4.5316;
+        const rateUSD = 3.7374;
+        const rateGBP = 5.2571;
 
         switch (currency) {
             case "EURO":
-                exchangeRate = rateEUR;
-                break
+                return rateEUR;
             case "USD":
-                exchangeRate = rateUSD;
-                break;
+                return rateUSD;
             case "GBP":
-                exchangeRate = rateGBP;
-                break;
+                return rateGBP;
         }
-        result = amountElement.value / exchangeRate;
-        exchangeRateElement.innerText = `${exchangeRate} PLN`;
-        resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-    });
+    }
 
+    const changeResult = (amount, currency) => {
+        return (amount / selectCurrency(currency));
+    }
+    const updateResultText = (amount, currency) => {
+        exchangeRateElement.innerText = `${selectCurrency(currency)} PLN`;
+        resultElement.innerText = `${changeResult(amount, currency).toFixed(2)} ${currency}`;
+    }
 
-    formElement.addEventListener("reset", () => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const amountElement = document.querySelector(".js-amount");
+        const currencySelectElement = document.querySelector(".js-currencySelect");
+        const amount = amountElement.value;
+        const currency = currencySelectElement.value;
+        updateResultText(amount, currency);
+    }
+
+    const onFormReset = () => {
         exchangeRateElement.innerText = "";
         resultElement.innerText = "";
-    });
+    }
+
+    const init = () => {
+        formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+        formElement.addEventListener("reset", onFormReset);
+    }
+
+    init();
+
+
+
 
 }
